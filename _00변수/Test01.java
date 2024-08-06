794c80ef9ab99489da3d2cc9de4c81d9a82aa844
kyoung <soos1298@naver.com>
2024-08-06 10:50:25 +0900

    00변수 미션 수행 완료

--- _00변수/Test01.java
+++ _00변수/Test01.java
@@ -4,6 +4,16 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 정수 변수 선언
+		int a =10;
+		byte aa =10;
+		System.out.println(a);
+		System.out.println(aa);
+		// 소스 실행 하는 단축키 : Ctrl + f11
+		
+		// 문자열 변수 선언
+		String s = "Kimtr";
+		System.out.println(s);
 
 	}
 
---
+++ _00변수/Test02.java
@@ -0,0 +1,13 @@
+package _00변수;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 오후 문제 풀이
+		// 주요기능 : 변수, 자료형, 연산자
+		// 문제 1
+
+	}
+
+}
