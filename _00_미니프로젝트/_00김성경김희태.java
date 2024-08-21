6152626d421a8d32f85d473d08221632ca99f04c
sungkyoung8575 <soos1298@naver.com>
2024-08-21 18:07:17 +0900

    미니프로젝트

--- _00_미니프로젝트/_00김성경김희태.java
+++ _00_미니프로젝트/_00김성경김희태.java
@@ -10,20 +10,35 @@ public class _00김성경김희태 {
 
 		Scanner in = new Scanner(System.in);
 		Random r= new Random();
-		String[] ko= {"빨강","노랑","파랑","초록","보라"};
-		String[] en= {"red","yellow","blue","green","purple"};
-		int[] a=new int[5];
+		String[] ko= {"몬스테라","산세베리아","스투키","필로덴드론","스파티필럼","관엽식물","로즈마리","바질","라벤더","팔손이"};
+		String[] en= {"red","yellow","blue","green","purple","Orange","Pink","Black","White","Gray"};
+		int[] a=new int[10];
 		int num=0;
+		int cnt=0;
 		
-		for(int i=0;i<5;i++) {
-			num=r.nextInt(5);
+		for(int i=0;i<10;i++) {
+			num=r.nextInt(10);
 			if(a[num]==0) {
 				a[num]=1;
 				System.out.println(ko[num]);
+				String word=in.nextLine();
+				if(ko[num].equals(word)) {
+					cnt+=10;
+					System.out.println("일치");
+					System.out.println("현재점수 "+cnt+"점");
+				}else {
+					System.out.println("불일치");
+					System.out.println("현재점수 "+cnt+"점");
+				}
 			}else {
 				i=i-1;
-			}
+			}		
 		}
+		
+		System.out.println("게임종료 총점수 "+cnt);
+		
+		
+	
 		
 
 		
--- _00_미니프로젝트/_test.java
+++ _00_미니프로젝트/_test.java
@@ -1,10 +1,21 @@
 package _00_미니프로젝트;
 
+import java.util.Random;
+
 public class _test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// test dsdfsddjflksjlkdjflk
+
+		// 랜덤한 4글자 문자열 만들기
+		
+	    String str = "1234MNO890ABCDEFGHIJKL567PQRSTUVWXYZSLK";
+	    Random random = new Random();
+	 
+	  for(int i=0;i<4;i++) {
+		  int num=random.nextInt(40);
+		  System.out.print(str.charAt(num));
+	  }
 		
 	}
 
